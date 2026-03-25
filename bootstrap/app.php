419d988000bd7d1e23acec1990b6d8dba33d61cb
Allen Gabrielle Cruiz <cruizallen2@gmail.com>
2026-03-26 03:06:17 +0800

    feat: implement inactivity timeout middleware; add session handling for user logout after inactivity

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\CheckRole;
 use App\Http\Middleware\EnsureSetupComplete;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\InactivityTimeout;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -23,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
             EnsureSetupComplete::class,
+            InactivityTimeout::class,
         ]);
 
         $middleware->alias([
